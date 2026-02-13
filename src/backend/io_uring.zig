c16162f0f9047f27d6230c548e1e25070b9be163
Buck McCready <jbuckmccready@gmail.com>
2026-02-13 10:00:16 -0800

    fix(io_uring): handle ENOTCONN error in shutdown path

--- src/backend/io_uring.zig
+++ src/backend/io_uring.zig
@@ -732,6 +732,7 @@ pub const Completion = struct {
             .shutdown => .{
                 .shutdown = if (res >= 0) {} else switch (@as(posix.E, @enumFromInt(-res))) {
                     .CANCELED => error.Canceled,
+                    .NOTCONN => error.SocketNotConnected,
                     else => |errno| posix.unexpectedErrno(errno),
                 },
             },
@@ -1083,6 +1084,7 @@ pub const ReadError = error{
 
 pub const ShutdownError = error{
     Canceled,
+    SocketNotConnected,
     Unexpected,
 };
 
